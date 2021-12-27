81a2cae0dc53d1b9b41f887951cd474219a6b4ea
manish0502 <manishgoswami495@gmail.com>
2021-12-27 19:10:58 +0530

    adding secret of JWrT

--- Building-Blog-Project-using-Nestjs/src/app.module.ts
+++ Building-Blog-Project-using-Nestjs/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -22,6 +23,7 @@ import { UserModule } from './user/user.module';
       
     }),
     UserModule,
+    AuthModule,
 
   ],
   controllers: [AppController],
---
+++ Building-Blog-Project-using-Nestjs/src/auth/auth.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable, from, of } from 'rxjs';
+
+@Injectable()
+export class AuthService {}
