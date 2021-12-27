3393eb48e2be5333f3dfaa33f64e90577d6e7a33
manish0502 <manishgoswami495@gmail.com>
2021-12-27 19:47:34 +0530

    bcrypt added

--- Building-Blog-Project-using-Nestjs/src/auth/auth.service.ts
+++ Building-Blog-Project-using-Nestjs/src/auth/auth.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Observable, from, of } from 'rxjs';
+const bcrypt = require('bcrypt');
+import { User }  from '../user/models/user-interface';
 
-@Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly jwtService: JwtService){}
+
+  generateJWT(user :User) : Observable<string>{
+    return from(this.jwtService.signAsync({user}))
+  }
+
+  hashPassword(passport:string): Observable<String>{
+     return 
+  }
+
+
+  comparePassword(): Observable<any>{
+    return 
+  }
+
+}
