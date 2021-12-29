5a24aed0a5c7f064793330836bc9f8220b0e2259
manish0502 <manishgoswami495@gmail.com>
2021-12-29 17:51:11 +0530

    access token added

--- Building-Blog-Project-using-Nestjs/src/auth/auth.service.ts
+++ Building-Blog-Project-using-Nestjs/src/auth/auth.service.ts
@@ -9,17 +9,43 @@ import { User }  from '../user/models/user-interface';
 export class AuthService {
     constructor(private readonly jwtService: JwtService){}
 
-  generateJWT(user :CreateUserDto) : Observable<string>{
-    return from(this.jwtService.signAsync({user}))
-  }
+//   generateJWT(user :CreateUserDto) : Observable<string>{
+//     return from(this.jwtService.signAsync({user}))
+//   }
 
-  hashPassword(passport:string): Observable<String>{
-     return from<string>(bcrypt.hash(passport ,12))
-  }
+//   hashPassword(passport:string): Observable<String>{
+//      return from<string>(bcrypt.hash(passport ,12))
+//   }
 
 
-  comparePassword(newPassword:string , hashPassword:string): Observable<any | boolean>{
-    return from(<any | boolean>bcrypt.comparePassword(newPassword ,hashPassword));
-  }
+//   comparePassword(password: string, passwortHash: string): Observable<any>{
+//     return from(bcrypt.compare(password, passwortHash));
+// }
+
+// hashPassword(password: string): Observable <string> {
+//   try{
+//     return from<string>(bcrypt.hashSync(password, 12));
+
+//   }
+// catch(err){
+//   console.error(err);
+// }
+// }
+
+
+
+
+generateJWT(user: User): Observable <string> {
+  return from(this.jwtService.signAsync({user}));
+}
+
+hashPassword(password: string): Observable <string> {
+ 
+    return from<string>(bcrypt.hash(password, 12));
+}
+
+comparePassword(newPassword: string, passwortHash: string): Observable<any>{
+  return of<any | boolean>(bcrypt.compare(newPassword, passwortHash));
+}
 
 }
